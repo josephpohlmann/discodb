a4c13f664b287dca66860fa687f85b466b7e6322
dan <dan@bauman.space>
2018-12-15 19:42:49 -0600

    merge code in py3

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup, Extension
-
+from setuptools import setup, Extension
 discodb_module = Extension('discodb._discodb',
                            sources=['discodbmodule.c'],
                            libraries=['discodb'])
 
 setup(name='discodb',
-      version='0.6',
+      version='0.7',
       description='An efficient, immutable, persistent mapping object.',
       author='Nokia Research Center',
       install_requires=[
